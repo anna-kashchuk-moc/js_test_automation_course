5abd612f9424693107dd477e8e69881f6fac404f
Serhii Husenko <sgusenko@gmail.com>
2025-06-21 20:39:37 +0300

    HW_2: Coding Task 2 by Serhii Husenko

---
+++ Husenko_Serhii/HW_2/CT_2/coding_task2.js
@@ -0,0 +1,32 @@
+let name = prompt("What is you name?").trim();
+let age = prompt("What is you age?").trim();
+let isWorker;
+let experience;
+
+if (isNaN(age) || age < 0) {
+    console.log(`${age} is not a number or less than 0`);
+} else {
+    isWorker = prompt("Do you work? (yes/no)").trim().toLowerCase();
+    if (isWorker !== "yes" && isWorker !== "no") {
+        console.log('Only "yes" or "no" answers');
+    } else {
+        if (age < 18) {
+            console.log("You are a minor");
+        } else if (age >= 18 && isWorker === "yes") {
+            console.log("You are an adult worker");
+
+            experience = prompt("How many years have you been working?").trim();
+            if (isNaN(experience) || experience < 0) {
+                console.log(`${experience} is not a number or less than 0`);
+            } else if (experience < 3) {
+                console.log("You are a beginner. Still a long way to go!");
+            } else if (experience >= 3 && experience <= 9) {
+                console.log("You are an experienced worker.");
+            } else {
+                console.log("You are a true pro in your field!");
+            }
+        } else {
+            console.log("You are an adult but not working yet");
+        }
+    }
+}
