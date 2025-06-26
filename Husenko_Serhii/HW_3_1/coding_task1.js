c5044eef38f9609c12139570fd51c648169e1405
Serhii Husenko <sgusenko@gmail.com>
2025-06-26 20:47:08 +0300

    HW_3: Coding Task 1 by Serhii Husenko

---
+++ Husenko_Serhii/HW_3_1/coding_task1.js
@@ -0,0 +1,13 @@
+const a = 6;
+const b = 8;
+
+function sum(a, b) {
+    return a + b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+console.log(`The sum of ${a} and ${b} is ${sum(6, 8)}`);
+console.log(`The multiplication of ${a} and ${b} is ${multiply(6, 8)}`);
